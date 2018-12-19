945b565da5ed5e26cc53c313a70c942b1e6508fc
Arlene Andrews <kara_arlene@yahoo.com>
2018-12-18 21:06:58 -0700

    added Jest and first test

---
+++ CatTestJest/index.android.test.js
@@ -0,0 +1,5 @@
+describe('index.android', () => {
+    //checks to insure that values of joke in inital state is empty
+    it("initialStateJokeIsNull", () => (
+        expect(joke).toEqual(null)))
+});
